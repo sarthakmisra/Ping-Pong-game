6008395c6eecce39e3d9124c60353c4099c05af1
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2022-07-23 14:15:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function show_instructions() {
+    document.getElementById("instructions").innerHTML = "<br><button id='hide_instructions' onclick='hide_instructions()' class='btn btn-default'>X</button><br><h3>Instructions</h3> <hr>  <p>1. First Keep your Laptop screen straight.</p> <p>2. Move yourself atleast 2-3 feet away from the laptop</p> <p>3. Move your right wrist in an ascending and descending manner , a red dot should appear on your right wrist</p> <p>4. Now press the play button , and again move yourself 3-4 feet away from laptop.</p> <p>5. The red paddel is your paddel that will move per your right wrist.</p><br><br><b>If you've read the instructions, press the 'x' on the top right and enjoy;)</b><br><br>";
+}
+function hide_instructions() {
+    document.getElementById("instructions").innerHTML="";
+}
+function play() {
+    
+}
